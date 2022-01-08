83f1e7bf94d5910b7fc5dabda0d9cb524215b39c
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-01-08 22:11:49 +0700

    feat: implement human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function toReadable (number) {
-  
+let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let tenToTwenty = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+function toNormalForm (number) {
+  let standartNum = '' + number;
+
+  while (standartNum.length <= 2) {
+    standartNum = '0' + standartNum;
+  }
+  return standartNum;
 }
+
+function toReadable (number) {
+  if (number === 0) {
+    return 'zero';
+  }
+
+  let standartNum = toNormalForm(number);
+  
+  let humanReadNumber = ''; 
+  if (Number(standartNum[0]) !== 0) {
+    humanReadNumber += units[Number(standartNum[0])] + ' hundred' + ' ';
+  } 
+
+  if (Number(standartNum[1]) === 1) {
+    humanReadNumber += tenToTwenty[Number('1'+ standartNum[2]) - 10];
+    return humanReadNumber.trim();
+  }
+
+  if (Number(standartNum[1]) !== 0 ) {
+    humanReadNumber += dozens[Number(standartNum[1])] + ' ';
+  } 
+
+  if (Number(standartNum[2]) !== 0) {
+    humanReadNumber += units[Number(standartNum[2])];
+  }
+
+  return humanReadNumber.trim();
+}
+
+module.exports = toReadable;
